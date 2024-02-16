ffb6c7e15027ee7b234331d462cd9f2088aa45b4
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-02-16 22:38:27 +0530

    Update

--- 12admindashboardapp/src/components/Charts/LineChart.jsx
+++ 12admindashboardapp/src/components/Charts/LineChart.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts'
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy'
 
 const LineChart = () => {
     return (
-        <div>LineChart</div>
+        <ChartComponent>
+
+        </ChartComponent>
     )
 }
 
--- 12admindashboardapp/src/components/Navbar.jsx
+++ 12admindashboardapp/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 
 const Navbar = () => {
-    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize, currentColor } = useStateContext();
 
     useEffect(() => {
         const handleResize = () => setScreenSize(window.innerWidth);
@@ -57,14 +57,14 @@ const Navbar = () => {
             <NavButton
                 title='Menu'
                 customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-                color='blue'
+                color={currentColor}
                 icon={<AiOutlineMenu />}
             />
             <div className="flex">
                 <NavButton
                     title='Cart'
                     customFunc={() => handleClick('cart')}
-                    color='blue'
+                    color={currentColor}
                     icon={<FiShoppingCart />}
                 />
 
@@ -72,7 +72,7 @@ const Navbar = () => {
                     title='Chat'
                     dotColor='#03C9D7'
                     customFunc={() => handleClick('chat')}
-                    color='blue'
+                    color={currentColor}
                     icon={<BsChatLeft />}
                 />
 
@@ -80,7 +80,7 @@ const Navbar = () => {
                     title='Notification'
                     dotColor='#03C9D7'
                     customFunc={() => handleClick('notification')}
-                    color='blue'
+                    color={currentColor}
                     icon={<RiNotification3Line />}
                 />
                 <TooltipComponent
