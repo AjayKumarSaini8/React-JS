45a556084411e448a147142991170b40498c89cb
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-02-16 22:41:21 +0530

    Update

--- 12admindashboardapp/src/components/Charts/LineChart.jsx
+++ 12admindashboardapp/src/components/Charts/LineChart.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip, LineSeries } from '@syncfusion/ej2-react-charts'
 import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy'
 
 const LineChart = () => {
     return (
         <ChartComponent>
-
+            <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
         </ChartComponent>
     )
 }
